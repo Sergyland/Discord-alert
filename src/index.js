972d26c67f8d2ba10d7adb1370ce639b9a1ecb6a
serge55555 <serge55555@hotmail.fr>
2021-05-17 21:17:27 +0200

    Reverted to CJS

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import * as core from '@actions/core';
-import { context } from '@actions/github';
-import * as Discord from 'discord.js'
-import dotenv from "dotenv"
+const core = require('@actions/core');
+const { context } = require('@actions/github');
+const Discord = require('discord.js');
+
 if(!process.env.GITHUB_ACTIONS){ 
-    dotenv.config()
+    require('dotenv').config()
 }
 
-export async function run() {
+async function run() {
     const channelID = core.getInput('channel-id', {required: true});
     const discordToken = core.getInput('discord-token', {required: true});
     //const githubToken = core.getInput('github-token', {required: true});
@@ -19,7 +19,7 @@ export async function run() {
     core.debug(JSON.stringify(context.payload,"2"))
 }
 
-export async function sendDiscordMessage(discordToken, channelID, messageContent) {
+async function sendDiscordMessage(discordToken, channelID, messageContent) {
     const client = new Discord.Client();
     client.login(discordToken);
 
